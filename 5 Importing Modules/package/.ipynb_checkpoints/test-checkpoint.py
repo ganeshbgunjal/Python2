f8a87d0cf20cbe8fecfbedc2477fd97cbaea5ce5
ganeshgunjal118@gmail.com <ganeshgunjal118@gmail.com>
2025-02-06 11:15:52 +0530

    function folder arranged

---
+++ 5 Importing Modules/package/.ipynb_checkpoints/test-checkpoint.py
@@ -0,0 +1,4 @@
+from package.math import *
+
+print(addition(4,5))
+print(multiply(4,5))
---
+++ 5 Importing Modules/test.py
@@ -0,0 +1,6 @@
+from package.math import *
+from package.subpackages.multi import *
+
+print(addition(4,5))
+print(multiply(5,6))
+print(multiplication(6,4))
